42b8672b1203cfc6103c7ee3489f68e9e35608cb
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-12-16 23:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // complete the given function
 
 function palindrome(str){
+	let s = "";
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i) >='a' && str.charAt(i) <='z'){
+			s += str.charAt(i);
+		}
+		else if(str.charAt(i) >='A' && str.charAt(i) <='Z'){
+			s += str.charAt(i).toLowerCase();
+		}
+		else if(str.charAt(i) == ' '){
+			i++;
+		}
+	}
+	for(let i=0;i<s.length/2;i++){
+		if(s.charAt(i) !== s.charAt(s.length-i-1)){
+			return false;
+		}
+	}
+	return true;
 
 }
 module.exports = palindrome
